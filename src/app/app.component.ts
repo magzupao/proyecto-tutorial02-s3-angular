31dc910e90fb0ffa46551b7ae150bd849b1d1f72
mguado <mguado@gmail.com>
2022-09-18 14:20:30 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'proyecto-tutorial02-s3-angular';
+}
